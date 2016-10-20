cd9da9e371d3968851b8f23e55c385b4daa8695d
JeanClaudeAber <jean_aber@hotmail.com>
2016-10-20 20:21:09 +0200

    Pushed All Files.

---
+++ MSS.MVC/MSS.MVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MSS.MVC.Startup))]
+namespace MSS.MVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
