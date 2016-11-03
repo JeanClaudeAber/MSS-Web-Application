55c020f3c14ddd286db8ed87ce5079ccd5f24c39
JeanClaudeAber <jean_aber@hotmail.com>
2016-11-03 13:17:52 +0100

    Changed Jumbotron text and renamed Boss Theme Files to BossThemeFiles.

--- MSS.MVC/MSS.MVC/App_Start/BundleConfig.cs
+++ MSS.MVC/MSS.MVC/App_Start/BundleConfig.cs
@@ -9,13 +9,13 @@ namespace MSS.MVC
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Boss Theme Files/js/jquery.min.js"));
+                        "~/BossThemeFiles/js/jquery.min.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
 
             bundles.Add(new ScriptBundle("~/bundles/queryLoader").Include(
-                        "~/Boss Theme Files/js/queryloader2.min.js"));
+                        "~/BossThemeFiles/js/queryloader2.min.js"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
@@ -27,31 +27,31 @@ namespace MSS.MVC
                       "~/Scripts/respond.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/bossScripts").Include(
-                      "~/Scripts//Boss Theme Files/js/jquery.hoverIntent.min.js",
-                      "~/Boss Theme Files/js/jquery.nicescroll.min.js",
-                      "~/Boss Theme Files/js/waypoints.min.js",
-                      "~/Boss Theme Files/js/waypoints-sticky.min.js",
-                      "~/Boss Theme Files/js/jquery.debouncedresize.js",
-                      "~/Boss Theme Files/js/retina.min.js",
-                      "~/Boss Theme Files/js/jquery.themepunch.tools.min.js",
-                      "~/Boss Theme Files/js/jquery.themepunch.revolution.min.js",
-                      "~/Boss Theme Files/js/owl.carousel.min.js",
-                      "~/Boss Theme Files/js/isotope.pkgd.min.js",
-                      "~/Boss Theme Files/js/skrollr.min.js",
-                      "~/Boss Theme Files/js/jquery.magnific-popup.min.js",
-                      "~/Boss Theme Files/js/jquery.countTo.js",
-                      "~/Boss Theme Files/js/jquery.validate.min.js",
-                      "~/Boss Theme Files/js/contact.js",
-                      "~/Boss Theme Files/js/main.js",
+                      "~/Scripts//BossThemeFiles/js/jquery.hoverIntent.min.js",
+                      "~/BossThemeFiles/js/jquery.nicescroll.min.js",
+                      "~/BossThemeFiles/js/waypoints.min.js",
+                      "~/BossThemeFiles/js/waypoints-sticky.min.js",
+                      "~/BossThemeFiles/js/jquery.debouncedresize.js",
+                      "~/BossThemeFiles/js/retina.min.js",
+                      "~/BossThemeFiles/js/jquery.themepunch.tools.min.js",
+                      "~/BossThemeFiles/js/jquery.themepunch.revolution.min.js",
+                      "~/BossThemeFiles/js/owl.carousel.min.js",
+                      "~/BossThemeFiles/js/isotope.pkgd.min.js",
+                      "~/BossThemeFiles/js/skrollr.min.js",
+                      "~/BossThemeFiles/js/jquery.magnific-popup.min.js",
+                      "~/BossThemeFiles/js/jquery.countTo.js",
+                      "~/BossThemeFiles/js/jquery.validate.min.js",
+                      "~/BossThemeFiles/js/contact.js",
+                      "~/BossThemeFiles/js/main.js",
                       "~/Custom Scripts/myScript.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Boss Theme Files/css/animate.css",
-                      "~/Boss Theme Files/css/bootstrap.min.css",
-                      "~/Boss Theme Files/css/magnific-popup.css",
-                      "~/Boss Theme Files/css/style.css",
-                      "~/Boss Theme Files/css/revslider/revslider-index33.css",
-                      "~/Boss Theme Files/css/colors/darkblue.css",
+                      "~/BossThemeFiles/css/animate.css",
+                      "~/BossThemeFiles/css/bootstrap.min.css",
+                      "~/BossThemeFiles/css/magnific-popup.css",
+                      "~/BossThemeFiles/css/style.css",
+                      "~/BossThemeFiles/css/revslider/revslider-index33.css",
+                      "~/BossThemeFiles/css/colors/darkblue.css",
                       "~/Content/font-awesome.min.css",
                       "~/Custom Styles/myStyles.css"));
         }
