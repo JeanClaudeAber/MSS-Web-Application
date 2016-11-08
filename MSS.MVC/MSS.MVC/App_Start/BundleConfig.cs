4abb22c95d22dcddf0e647be4c35d4b154c14418
JeanClaudeAber <jean_aber@hotmail.com>
2016-11-08 22:42:02 +0100

    Changed Structure

--- MSS.MVC/MSS.MVC/App_Start/BundleConfig.cs
+++ MSS.MVC/MSS.MVC/App_Start/BundleConfig.cs
@@ -8,52 +8,52 @@ namespace MSS.MVC
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/BossThemeFiles/js/jquery.min.js"));
+            //bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+            //            "~/BossThemeFiles/js/jquery.min.js"));
 
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
+            //bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+            //            "~/Scripts/jquery.validate*"));
 
-            bundles.Add(new ScriptBundle("~/bundles/queryLoader").Include(
-                        "~/BossThemeFiles/js/queryloader2.min.js"));
+            //bundles.Add(new ScriptBundle("~/bundles/queryLoader").Include(
+            //            "~/BossThemeFiles/js/queryloader2.min.js"));
 
-            // Use the development version of Modernizr to develop with and learn from. Then, when you're
-            // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
+            //// Use the development version of Modernizr to develop with and learn from. Then, when you're
+            //// ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
+            //bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+            //            "~/Scripts/modernizr-*"));
 
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
+            //bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+            //          "~/Scripts/bootstrap.js",
+            //          "~/Scripts/respond.js"));
 
-            bundles.Add(new ScriptBundle("~/bundles/bossScripts").Include(
-                      "~/Scripts//BossThemeFiles/js/jquery.hoverIntent.min.js",
-                      "~/BossThemeFiles/js/jquery.nicescroll.min.js",
-                      "~/BossThemeFiles/js/waypoints.min.js",
-                      "~/BossThemeFiles/js/waypoints-sticky.min.js",
-                      "~/BossThemeFiles/js/jquery.debouncedresize.js",
-                      "~/BossThemeFiles/js/retina.min.js",
-                      "~/BossThemeFiles/js/jquery.themepunch.tools.min.js",
-                      "~/BossThemeFiles/js/jquery.themepunch.revolution.min.js",
-                      "~/BossThemeFiles/js/owl.carousel.min.js",
-                      "~/BossThemeFiles/js/isotope.pkgd.min.js",
-                      "~/BossThemeFiles/js/skrollr.min.js",
-                      "~/BossThemeFiles/js/jquery.magnific-popup.min.js",
-                      "~/BossThemeFiles/js/jquery.countTo.js",
-                      "~/BossThemeFiles/js/jquery.validate.min.js",
-                      "~/BossThemeFiles/js/contact.js",
-                      "~/BossThemeFiles/js/main.js",
-                      "~/CustomScripts/myScripts.js"));
+            //bundles.Add(new ScriptBundle("~/bundles/bossScripts").Include(
+            //          "~/Scripts//BossThemeFiles/js/jquery.hoverIntent.min.js",
+            //          "~/BossThemeFiles/js/jquery.nicescroll.min.js",
+            //          "~/BossThemeFiles/js/waypoints.min.js",
+            //          "~/BossThemeFiles/js/waypoints-sticky.min.js",
+            //          "~/BossThemeFiles/js/jquery.debouncedresize.js",
+            //          "~/BossThemeFiles/js/retina.min.js",
+            //          "~/BossThemeFiles/js/jquery.themepunch.tools.min.js",
+            //          "~/BossThemeFiles/js/jquery.themepunch.revolution.min.js",
+            //          "~/BossThemeFiles/js/owl.carousel.min.js",
+            //          "~/BossThemeFiles/js/isotope.pkgd.min.js",
+            //          "~/BossThemeFiles/js/skrollr.min.js",
+            //          "~/BossThemeFiles/js/jquery.magnific-popup.min.js",
+            //          "~/BossThemeFiles/js/jquery.countTo.js",
+            //          "~/BossThemeFiles/js/jquery.validate.min.js",
+            //          "~/BossThemeFiles/js/contact.js",
+            //          "~/BossThemeFiles/js/main.js",
+            //          "~/CustomScripts/myScripts.js"));
 
-            bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/BossThemeFiles/css/animate.css",
-                      "~/BossThemeFiles/css/bootstrap.min.css",
-                      "~/BossThemeFiles/css/magnific-popup.css",
-                      "~/BossThemeFiles/css/style.css",
-                      "~/BossThemeFiles/css/revslider/revslider-index33.css",
-                      "~/BossThemeFiles/css/colors/darkblue.css",
-                      "~/Content/font-awesome.min.css",
-                      "~/CustomStyles/myStyles.css"));
+            //bundles.Add(new StyleBundle("~/Content/css").Include(
+            //          "~/BossThemeFiles/css/animate.css",
+            //          "~/BossThemeFiles/css/bootstrap.min.css",
+            //          "~/BossThemeFiles/css/magnific-popup.css",
+            //          "~/BossThemeFiles/css/style.css",
+            //          "~/BossThemeFiles/css/revslider/revslider-index33.css",
+            //          "~/BossThemeFiles/css/colors/darkblue.css",
+            //          "~/Content/font-awesome.min.css",
+            //          "~/CustomStyles/myStyles.css"));
         }
     }
 }
