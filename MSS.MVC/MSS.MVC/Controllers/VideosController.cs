ec8c0ef0d1e38fb02498ce47844ac526a523eb63
JeanClaudeAber <jean_aber@hotmail.com>
2016-11-15 19:57:20 +0100

    -Updated Footer + links -Added 404 page but not redirecting. -Added new controller + View for All Videos Page.

---
+++ MSS.MVC/MSS.MVC/Controllers/VideosController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MSS.MVC.Controllers
+{
+    public class VideosController : Controller
+    {
+        // GET: Videos
+        public ActionResult AllVideos()
+        {
+            return View();
+        }
+    }
+}
